064bf5bf4d2abe362f24106c60bcb2724664b55e
ntuthuko <ntuthukorocks@gmail.com>
2018-08-16 19:09:12 +0200

    division added

---
+++ CalculatorApp.Core/Division.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Core
+{
+    public class Division : ICalculator
+    {
+        public double Calculate(double Num1, double Num2)
+        {
+                return Num1 / Num2;
+        }
+
+        public int Calculate(int Num1, int Num2)
+        {
+                return Num1 / Num2;
+            
+        }
+    }
+}
---
+++ CalculatorApp.Testing/DivisionTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorApp.Core;
+
+namespace CalculatorApp.Testing
+{
+    [TestClass]
+    public class DivisionTest
+    {
+        [TestMethod]
+        [DataRow(20,4,5)]
+        [DataRow(0,40,0)]     
+        public void DivideIntTest(int num1, int num2, int expected)
+        {
+            //arrange:
+            int result = 0;
+
+            Division obj = new Division();
+
+            //act:
+            result = obj.Calculate(num1, num2);
+
+            //assert:
+            string strMessage = string.Format("Input: {0} - {1} = {2}. Expected: {3}",
+                num1, num2, result, expected
+                );
+            Assert.AreEqual(expected, result, strMessage);
+        }
+
+        [TestMethod]
+        [DataRow(8.5, 0.5, 17)]
+        public void DivideDoubleTest(double num1, double num2, double expected)
+        {
+            //arrange:
+            double result = 0;
+
+            Division obj = new Division();
+
+            //act:
+            result = obj.Calculate(num1, num2);
+
+            //assert:
+            string strMessage = string.Format("Input: {0} - {1} = {2}. Expected: {3}",
+                num1, num2, result, expected
+                );
+            Assert.AreEqual(expected, result, strMessage);
+        }
+    }
+}
